36dd03bf18b29f734ae0181aa5906923fc8ea88b
harshlalawat <harshlalawathlhl@gmail.com>
2023-04-03 23:34:29 +0530

    environment var in security

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -5,6 +6,8 @@ const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 
 const app = express();
+
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -16,8 +19,8 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-const secret = "Thisisalittlesecretkey.";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"]});
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = mongoose.model("User", userSchema);
 
