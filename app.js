126b27a6c7e5362d4eda833ceb01aa1dccaca440
harshlalawat <harshlalawathlhl@gmail.com>
2023-04-04 11:43:21 +0530

    Level 3 of security

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
+
 
 const User = mongoose.model("User", userSchema);
 
@@ -42,7 +42,7 @@ app.get("/register",function(req, res){
 app.post("/register", function(req, res){
   const user = new User({
     email: req.body.username,
-    password: req.body.password});
+    password: md5(req.body.password)});
   user.save().then(function(){
     res.render("secrets");
   });
@@ -51,7 +51,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   User.findOne({email: username}).then(function(result){
     if(result){
       if(result.password === password){
